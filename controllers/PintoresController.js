bc601e07715c47de138a3eceb141cd06677cdd92
AmericaGonzalez65 <64876990+AmericaGonzalez65@users.noreply.github.com>
2020-06-03 23:18:41 -0600

    Add files via upload

---
+++ controllers/PintoresController.js
@@ -0,0 +1,26 @@
+let mongoose =  require('mongoose');
+//Vamos a unirlo al modelo
+let Pintores = require('../models/Pintores')
+
+let pintoresController = {};
+
+/* Listar -> FIND() */
+pintoresController.list = (req, res) => {
+    Pintores.find({})
+    .limit(20)
+    .skip(0)
+    .exec((err, pintor)=>{
+        if (err){
+            console.log('Error:',err);
+        }
+        //console.log('Datos obtenidos');
+        //console.log(estatal);
+        res.render('../views/index',{
+            pintores: pintor,
+            title: "Listado de pintores",
+            year: new Date().getFullYear()
+        })
+    })
+};
+
+module.exports = pintoresController;
